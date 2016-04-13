b032da1a27f1711ae1c44d7dfc1ec9dd03322c5b
Nikos Nikoloutsakos <nikoloutsa@ceid.upatras.gr>
2016-04-13 14:07:13 +0300

    test base title

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
+
+  def setup
+	 @base_title = ""
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-	assert_select "title","Home"
+	assert_select "title","#{@base_title}Home"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-	assert_select "title","Help"
+	assert_select "title","#{@base_title}Help"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-	assert_select "title","About"
+	assert_select "title","#{@base_title}About"
   end
 
 end
