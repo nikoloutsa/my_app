6a49b1fe309ab4f4485f27a7dd185fc89ffca93a
Nikos Nikoloutsakos <nikoloutsa@ceid.upatras.gr>
2016-04-13 18:19:11 +0300

    added signup pages+tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'help' => 'pages#help'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
+  get 'signup' => 'users#new'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
