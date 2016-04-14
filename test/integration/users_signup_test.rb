7ba2860d8b331bc59fcaf3ff1ebf6fa64a045dd9
Nikos Nikoloutsakos <nikoloutsa@ceid.upatras.gr>
2016-04-14 17:13:47 +0300

    stash

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 							password_confirmation: "bar" }
 	  end
 	  assert_template 'users/new'
+	  assert_select 'div#error_explanation'
+	  assert_select 'div.field_with_errors'
    end
 
    test "valid signup information" do
@@ -23,6 +25,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 										 password_confirmation: "password" }
 	  end
 	  assert_template 'users/show'
+	  #assert_not flash.nil?
+	  assert_not_nil flash
    end
   # test "the truth" do
   #   assert true
