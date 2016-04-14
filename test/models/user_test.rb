31bf596179faea185866c1b3f31de93caa20c23c
Nikos Nikoloutsakos <nikoloutsa@ceid.upatras.gr>
2016-04-14 13:34:04 +0300

    validate secure_password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
    def setup
-	  @user = User.new(name: "Example User", email: "user@example.com")
+	  @user = User.new(name: "Example User", email: "user@example.com", password: "123456", password_confirmation: "123456")
    end
 
    test "shoud be valid" do
@@ -51,6 +51,11 @@ class UserTest < ActiveSupport::TestCase
 	  duplicate_user.email = @user.email.upcase
 	  @user.save
 	  assert_not duplicate_user.valid?
+   end
+
+   test "password should have a minimum length" do
+	  @user.password = @user.password_confirmation = "a"*5
+	  assert_not @user.valid?
    end
 
 
