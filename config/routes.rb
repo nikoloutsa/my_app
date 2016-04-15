9347014f936fc6b02b57beec721c6db4daa056a0
Nikos Nikoloutsakos <nikoloutsa@ceid.upatras.gr>
2016-04-15 12:18:23 +0300

    loginout

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 
